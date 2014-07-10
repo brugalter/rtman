7ba68914e8f33b77e9fc5b7f6843b53d300cff0f
Ryan Holman <k.ryan.holman@gmail.com>
2014-07-09 22:45:34 -0600

    added config.pl to create rtman.conf

--- config.pl
+++ config.pl
@@ -7,6 +7,25 @@ use warnings;
 ## creates /usr/local/bin/rtman
 ## adds config file /etc/rtman.conf
 
-open (CONFIG, '>>');
-print CONFIG "this should work";
-close (CONFIG);
+#write data to the config file
+sub wd  {
+  open (CONFIG, '>>rtman.conf');
+  print CONFIG "@_\n";
+  close (CONFIG);
+}
+
+sub create_config  {
+ wd '#rtman.conf';
+ wd '#written by brugalter';
+ wd();
+ wd '#move this file to /etc when you\'re done editing it';
+ wd();
+ wd '#host name or ip';
+ wd 'host=';
+ wd();
+ wd '#user name on remote host';
+ wd 'user=';
+}
+
+## create the config file in the current dir
+create_config();
